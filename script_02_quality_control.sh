098d75d2cbb834aa7841a4a12805779112c06e21
kim-123-hub <kimberley.c.williams@gmail.com>
2025-09-30 21:21:56 +0200

    Update script_02_quality_control.sh

--- script_02_quality_control.sh
+++ script_02_quality_control.sh
@@ -6,7 +6,7 @@
 # Usage: bash 02_quality_control.sh
 
 # Set up environment variables
-export FMRISTUDY="your_study_name"  # Change this to your actual study name
+export FMRISTUDY="connexin_bootcamp_analysis"  # Change this to your actual study name
 export BIDSOUTPUT="BIDS_output"
 export MRIQCOUTPUT="MRIQC_output"
 
